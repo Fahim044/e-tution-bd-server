4f7b51c75b65e0f283b4261d7838e074e38426ef
Fahim044 <fahiimfaysalleusc044@gmail.com>
2025-12-07 19:24:10 +0600

    added post users

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app=express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+app.use(express.json());
+app.use(cors());
+const port=process.env.PORT || 4000;
+const uri = `mongodb+srv://${process.env.db_username}:${process.env.db_password}@cluster0.gpbclj8.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+app.get('/',(req,res)=>{
+res.send('E-TUTION BD Server is Running');
+});
+
+async function run() {
+  try {
+    const db=client.db('E_Tution_BD_DB');
+    const usersCollection=db.collection('users');
+
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // users api
+    app.post('/users',async(req,res)=>{
+        const userInfo=req.body;
+        userInfo.createdAt=new Date().toLocaleString();
+        const userEmail=userInfo.email;
+        console.log('user Email: ',userEmail);
+        const userExists=await usersCollection.findOne({email:userEmail});
+        console.log('userExists: ',userExists);
+        if(userExists)
+        {
+            return res.send({message:'User Already exists.No need to save it again to the database'});
+        }
+        const result=await usersCollection.insertOne(userInfo);
+        res.send(result);
+    })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port,()=>{
+    console.log(`The Server is running at port : ${port}`);
+})
